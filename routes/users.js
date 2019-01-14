72d645ce3680895fa271b400c5407fc16a4d45d4
Publikwerker <jason.michael.hoffman@gmail.com>
2019-01-14 13:02:03 -0800

    created route for users

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/', (req, res, next) => {
+  const { username, fullname, password } = req.body;
+
+  if (!username) {
+    const err = new Error ('Missing `username` in request body');
+    err.status = 400;
+    return next (err);
+  }
+
+  const newUser = {
+    username,
+    fullname,
+    password
+  };
+  
+  User.create(newUser)
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201)
+      .json(result);
+    })
+    .catch(err => {
+      next(err);
+    
+    });
+
+
+});
+
+module.exports = router;
