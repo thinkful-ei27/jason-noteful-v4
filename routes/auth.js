7c835dc079c61cb858eafc047a36c821d1a533ca
Publikwerker <jason.michael.hoffman@gmail.com>
2019-01-15 12:11:04 -0800

    we have tokens

--- routes/auth.js
+++ routes/auth.js
@@ -3,16 +3,26 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET, JWT_EXPIRY } = require('../config');
+
+function createAuthToken (user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
 
 const options = {
-    session: false,
-    failWithError: true
+  session: false,
+  failWithError: true
 };
 
 const localAuth = passport.authenticate('local', options);
 
 router.post('/login', localAuth, function (req, res) {
-    return res.json(req.user);
+  const authToken = createAuthToken(req.user);
+  return res.json({ authToken });
 });
 
 
